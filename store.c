b10648e3c81d4b84d8de3dfd032f2d82fcb474ce
Nathan <nmcbride@protonmail.com>
2020-02-12 16:19:52 -0500

    Basic reading

--- store.c
+++ store.c
@@ -2,27 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define TRUE 1
 #define FALSE 0
+#define database "/home/COIS/3380/lab2/grocery_items.db"
 
 void display();
 
 int main(int argc, char *argv[]) {
     
-    int item_list;
+    int fd_item_list;
 
     if(argc < 2) {
         printf("This program requires an item list, exiting...\n");
         exit(1);
     }
 
-    if((item_list=open(argv[2], O_RDONLY)) == -1) {
+    if((fd_item_list=open(argv[1], O_RDONLY)) == -1) {
         printf("Could not open item list, exiting...\n");    
         exit(1);
     } else {
-        printf("File opened");
+        printf("File opened: %s\n", argv[1]);
     }
+
+    int last_byte = lseek(fd_item_list, 0, SEEK_END);
+    int position;
+    char vals[512];
+    for (position = 0; position <= last_byte; ++position) {
+        int curr_byte = lseek(fd_item_list, position, SEEK_SET);
+        read(fd_item_list, &vals[position], curr_byte);
+    }
+
+    printf("READ: %s\n", &vals);
 
     struct sku {
         char description[40];
