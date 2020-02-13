bbcbaf1a90a41728ccaa78aaa3150a67e2520894
Nathan <nmcbride@protonmail.com>
2020-02-13 15:51:22 -0500

    It works

--- store.c
+++ store.c
@@ -9,10 +9,6 @@
 #define FALSE 0
 #define DATABASE "/home/COIS/3380/lab2/grocery_items.db"
 
-
-
-void display(int itemsPurchased);
-
 struct sku {
     char description[40];
     char unitofmeasure[15];
@@ -28,12 +24,14 @@ struct item {
 
 int main(int argc, char *argv[]) {
     
+    int itemsPurchased = 0;
+    float totalPrice = 0.0;
+
     // Check if the user supplied a item list
     if(argc < 2) {
         printf("This program requires an item list, exiting...\n");
         exit(1);
     }
-
 
     // Open the item list for reading
     FILE *item_list;
@@ -42,57 +40,58 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Could not open item list, exiting...\n");    
         exit(1);
     } 
-    else { printf("File opened: %s\n", argv[1]); }
- 
 
     // Open the database for reading
     FILE *db;
-    db = fopen (DATABASE, "R");
-    if(db = NULL) {
+    db = fopen (DATABASE, "r");
+    if(db == NULL) {
         fprintf(stderr, "Could not open database, exiting...\n");
         exit(1);
     } 
-    else { printf("database opened\n"); }
     
     // Taken from: https://stackoverflow.com/questions/1442116/how-to-get-the-date-and-time-values-in-a-c-program
     time_t t = time(NULL);
     struct tm time = *localtime(&t);
-    printf("===================================================================\n");
-    printf("                      Welcome to Food Mart!                        \n");
-    printf(" Date: %d-%02d-%02d %02d:%02d                                      \n", time.tm_year + 1900, time.tm_mon + 1, time.tm_mday, time.tm_hour, time.tm_min );
-    printf("                        Unit of        Unit       # of    Extended \n");
-    printf(" Description            Measure        Price      items     Price  \n");
-    printf(" ------------           -------        -----      -----    ------- \n");
+    printf("===================================================================================\n\n");
+    printf("                             Welcome to Food Mart!                           \n");
+    printf(" Date: %d-%02d-%02d %02d:%02d                                                \n", time.tm_year + 1900, time.tm_mon + 1, time.tm_mday, time.tm_hour, time.tm_min );
+    printf("                                        Unit of        Unit       # of    Extended \n");
+    printf(" Description                            Measure        Price      items     Price  \n");
+    printf(" -----------                            -------        -----      -----    ------- \n");
     
     // Count the number of items being purchased in the list
-    int itemsPurchased = 0;
-    while(!feof(item_list)){
+    while(!feof(item_list)) 
+    {
 	char ch = fgetc(item_list);
-	if(ch == '\n'){
-	    itemsPurchased++;
-	}
+	if(ch == '\n') { itemsPurchased++; }
     }
     rewind(item_list);
-
- 
+    
     int counter;
     for(counter = 0; counter < itemsPurchased; counter++) {
         struct item currItem = {{"", "", 0.0}, 0, 0};
-        fscanf(item_list, "%d %d", &currItem.id, &currItem.quantity);
-        //printf("curritem.id: %d\ncurritem.quantity: %d\n", currItem.id, currItem.quantity);
-	
-	int db_index = currItem.id * sizeof(struct sku);
-	lseek(db, db_index, SEEK_SET);
-	read(db, currItem.info, sizeof(struct sku));
 
-	printf("info.des: %s\ninfo.uom: %s\nprice: %f\n", currItem.info.description, currItem.info.unitofmeasure, currItem.info.price);
- 
+        fscanf(item_list, "%d %d", &currItem.id, &currItem.quantity);
+	int db_index = currItem.id * sizeof(struct sku);
+
+	fseek(db, db_index, SEEK_SET);
+
+	fread(&currItem.info, sizeof(struct sku), 1, db);
+
+	printf(" %-40s", currItem.info.description);
+	printf("%-15s", currItem.info.unitofmeasure);
+	printf("%-12.2f", currItem.info.price);
+	printf("%-8d", currItem.quantity);
+	printf("%-15.2f\n", currItem.quantity * currItem.info.price);
+
+	totalPrice = totalPrice + currItem.info.price;
     } 
 
-
-    printf("       Thank you for purchasing %d items from our fine store.      \n", itemsPurchased);
-    printf("                        Have a great day eh?                       \n");
-    printf("===================================================================\n");
+    printf("							            -------------     \n");
+    printf("								    Total: %.2f     \n\n", totalPrice);
+    printf("              Thank you for purchasing %d items from our fine store.               \n", itemsPurchased);
+    printf("                            Have a great day eh?                                   \n");
+    printf("===================================================================================\n");
 
     return 0;
 }
